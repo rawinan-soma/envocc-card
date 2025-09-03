e8d1390571c7417874dc6775b2be1f79b927a910
rawinan-s <rawinan.soma@gmail.com>
2025-09-03 21:58:53 +0700

    add files to admin controller

--- src/public/public.controller.ts
+++ src/public/public.controller.ts
@@ -1,28 +1,11 @@
-import { Controller, Get, Param, Query, Req } from '@nestjs/common';
+import { Controller } from '@nestjs/common';
 import { PublicService } from './public.service';
-import { FileModelMap, FilesService } from 'src/files/files.service';
-import type { RequestwithAdminData } from 'src/admin-auth/request-admin.interface';
-import type { RequestwithUserData } from 'src/user-auth/request-user-interface';
+import { FilesService } from 'src/files/files.service';
 
-@Controller('shared')
+@Controller()
 export class PublicController {
   constructor(
     private readonly publicService: PublicService,
     private readonly filesService: FilesService,
   ) {}
-
-  @Get('files/:file')
-  async getFilesHandler(
-    @Param() file: keyof FileModelMap,
-    @Query('id') id?: number,
-    @Req() adminReq?: RequestwithAdminData,
-    @Req() userReq?: RequestwithUserData,
-  ) {
-    // let id: number;
-    if (!id && userReq && !adminReq) {
-      id = userReq.user.id;
-    }
-
-    return await this.filesService.getFileByUserId(file, id as number);
-  }
 }
