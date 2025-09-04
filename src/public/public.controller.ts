21a69d1b57e82d2f5261452d4604e850f310473c
rawinan-s <rawinan.soma@gmail.com>
2025-09-04 23:05:22 +0700

    temp

--- src/public/public.controller.ts
+++ src/public/public.controller.ts
@@ -1,11 +1,7 @@
 import { Controller } from '@nestjs/common';
 import { PublicService } from './public.service';
-import { FilesService } from 'src/files/files.service';
 
 @Controller()
 export class PublicController {
-  constructor(
-    private readonly publicService: PublicService,
-    private readonly filesService: FilesService,
-  ) {}
+  constructor(private readonly publicService: PublicService) {}
 }
