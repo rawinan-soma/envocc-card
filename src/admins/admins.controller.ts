ea9e5ecd0c1788464089d67af70ffde5b01043ff
rawinan-s <rawinan.soma@gmail.com>
2025-09-23 11:31:35 +0700

    edit controller

--- src/admins/admins.controller.ts
+++ src/admins/admins.controller.ts
@@ -41,7 +41,7 @@ export class AdminsController {
 
   @Get('admins')
   async getAdminHandler(
-    @Query('id', ParseIntPipe) id?: number,
+    @Query('id') id?: number,
     @Query('username') username?: string,
     @Query('email') email?: string,
   ) {
